03b57f3af9a50883a294446971529aae76c068d3
krimisha <krimisha025@gmail.com>
2023-12-28 17:47:26 +0000

    program

---
+++ loopq6.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+main(){
+	
+	char i, j;
+	char k='A';
+	
+	for(i='A'; i<='E'; i++){
+		for(j='A'; j<='E'; j++){
+			printf("%c", k);	
+			k++;			
+		}
+		
+		printf("\n");
+		
+		
+	}
+
+
+}
